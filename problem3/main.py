035016091d42067424f415816ad9e6081c7b87db
CharisChakim <m.charischakim@gmail.com>
2023-09-28 15:08:28 +0700

    Tugas Part 2

--- problem3/main.py
+++ problem3/main.py
@@ -4,3 +4,6 @@ r = 4.0
 
 
 # kode disini
+pi = 3.14
+surface_area= 2*pi*r*(r+T)
+print(surface_area)
--- problem4/main.py
+++ problem4/main.py
@@ -3,3 +3,21 @@ Input Harga: 370000
 Input Diskon: 15
 Output: harga yang harus dibayar adalah Rp. 314.500
 '''
+#Import Module
+import locale
+
+# Set locale sesuai dengan konvensi negara yang Anda inginkan (Indonesia)
+locale.setlocale(locale.LC_ALL, 'id_ID')
+
+#input harga
+harga_awal = int(input("Input Harga:"))
+diskon = int(input("Input Diskon:"))
+
+#Membuat rumus harga setelah diskon
+final_price = harga_awal-(harga_awal*(diskon/100))
+
+#Mengubah hasil agar menjadi bentuk yang diinginkan
+format_price = locale.format_string("%.0f", final_price, grouping=True)
+
+#Output
+print("harga yang harus dibayar adalah Rp.",format_price)
